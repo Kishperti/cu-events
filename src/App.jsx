82cb05b1a9f09ef3528086d468b01089f8c62134
Anshika <anshikathakur596@gmail.com>
2023-12-18 22:12:10 +0530

    landing component created

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import NavBar from "./component/NavBar";
 import About from './component/About';
 import Footer from "./component/Footer";
 import Events from './component/Events';
+import Landing from "./component/Landing";
 
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Landing/>
       <About/>
       <Events/>
       <Footer/>
