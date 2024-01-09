b1fa2625089c6130e8e6e9bfed590d3f823b52fa
Anshika <anshikathakur596@gmail.com>
2024-01-09 21:43:27 +0530

    Update About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
       <img src={tri} alt="" className='absolute top-0 left-[50%] -translate-x-1/2' />
       <h1 className='text-[#dddddd] text-6xl font-bold text-cente font-oswald text-center mb-4'>ABOUT <span className='text-[#E2012D]'>CAC</span></h1>
       <div className='text-[#E2012D] text-4xl flex justify-center items-center'><MdKeyboardDoubleArrowDown /></div>
-      <div className="flex flex-row items-center justify-center w-full relative mt-20 h-full">
-        <img src={about} alt="" className="h-[90vh] w-1/2" />
-        <div className="grid place-items-center px-8 rounded-lg w-[60%] -ml-[20%] bg-[#000000d4]">
+      <div className="flex flex-row items-center justify-center w-full relative mt-6 lg:mt-20 h-full">
+        <img src={about} alt="" className="h-[90vh] w-1/2 hidden lg:block" />
+        <div className="grid place-items-center px-2 rounded-lg w-[90%] lg:w-[60%] lg:-ml-[20%] bg-[#000000d4]">
           <p className="text-lg lg:text-xl py-8 text-justify w-full px-7 leading-8 lg:px-12 text-white opacity-80 font-maven z-20">
             Chandigarh University Academic Competitions, where innovation converges with education to create an unparalleled academic spectacle. Our team is a dynamic collective of visionaries, strategists, and go-getters who are dedicated to orchestrate events that transcend conventional boundaries. We are an ensemble of dedicated professionals, passionate about creating transformative experiences through hackathons, workshops, and various academic events
             <br />
