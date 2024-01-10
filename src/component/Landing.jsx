42fd5d8ac0bf77668ebda05785732b3db4668c5a
Anshika <anshikathakur596@gmail.com>
2024-01-10 16:26:06 +0530

    stats added

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -14,20 +14,19 @@ export default function App() {
     return (
         <>
             <Swiper
-                spaceBetween={30}
                 centeredSlides={true}
                 autoplay={{
                     delay: 2500,
                     disableOnInteraction: false,
                 }}
                 modules={[Autoplay]}
-                className="mySwiper relative w-full"
+                className="mySwiper w-full h-[30vh] md:h-[60vh] lg:h-[90vh]"
             >
-                <SwiperSlide className='w-full h-[90vh] flex flex-col swiper-slide1'>
-                    <img className='w-full h-full' src={event1} alt="" />
-                </SwiperSlide>
-                <SwiperSlide className='w-full h-[90vh] flex flex-col swiper-slide1'>
+                <SwiperSlide className='w-full h-full flex flex-col swiper-slide1'>
                     <img className='w-full h-full' src={event2} alt="" />
+                </SwiperSlide>
+                <SwiperSlide className='w-full h-full flex flex-col swiper-slide1'>
+                    <img className='w-full h-full' src={event1} alt="" />
                 </SwiperSlide>
             </Swiper>
         </>
--- src/component/Stats.jsx
+++ src/component/Stats.jsx
@@ -4,20 +4,20 @@ export default function Stats() {
     return (
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 w-full py-[50px] px-[20px] bg-[#0e0d0d]">
             <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl lg:text-5xl font-medium text-[#E2012D] font-oswald'>10k +</h1>
-                <div className="text-2xl font-light text-white font-maven w-full mt-4 text-center">Total Participation</div>
+                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>10k +</h1>
+                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">Total Participation</div>
             </div>
             <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl lg:text-5xl font-medium text-[#E2012D] font-oswald'>300 +</h1>
-                <div className="text-2xl font-light text-white font-maven w-full mt-4 text-center">Achievements</div>
+                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>300 +</h1>
+                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">Achievements</div>
             </div>
             <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl lg:text-5xl font-medium text-[#E2012D] font-oswald'>7k +</h1>
-                <div className="text-2xl font-light text-white font-maven w-full mt-4 text-center">National Participation</div>
+                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>7k +</h1>
+                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">National Participation</div>
             </div>
             <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl lg:text-5xl font-medium text-[#E2012D] font-oswald'>3k +</h1>
-                <div className="text-2xl font-light text-white font-maven w-full mt-4 text-center">International Participation</div>
+                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>3k +</h1>
+                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">International Participation</div>
             </div>
         </div>
     )
