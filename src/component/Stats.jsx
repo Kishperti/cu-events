130a7744a04acce1804bdd60387c61a7426ba04a
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2024-01-13 15:54:19 +0530

    Update Stats.jsx

--- src/component/Stats.jsx
+++ src/component/Stats.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 export default function Stats() {
-    return (
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 w-full py-[50px] px-[20px] bg-[#0e0d0d]">
-            <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>10000+</h1>
-                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">Total Participation</div>
-            </div>
-            <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>500+</h1>
-                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">Achievements</div>
-            </div>
-            <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>9000+</h1>
-                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">National Participation</div>
-            </div>
-            <div className="flex flex-col w-full items-center justify-center">
-                <h1 className='text-xl md:text-5xl font-medium text-[#E2012D] font-oswald'>1000+</h1>
-                <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">International Participation</div>
-            </div>
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
+  const [count4, setCount4] = useState(0);
+
+  const startCounting = (isVisible, setCountFunction, finalCount, step = 10, interval = 50) => {
+    if (isVisible && setCountFunction && finalCount > 0) {
+      let currentCount = 0;
+      const intervalId = setInterval(() => {
+        currentCount += Math.floor(Math.random() * step);
+        setCountFunction(currentCount);
+        if (currentCount >= finalCount) {
+          clearInterval(intervalId);
+        }
+      }, 0);
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 w-full py-[50px] px-[20px] bg-[#0e0d0d]">
+      <VisibilitySensor
+        onChange={(isVisible) => startCounting(isVisible, setCount1, 10000,)}
+        partialVisibility
+      >
+        <div className="flex flex-col w-full items-center justify-center">
+          <h1 className="text-xl md:text-5xl font-medium text-[#E2012D] font-oswald">{count1}</h1>
+          <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">
+            Total Participation
+          </div>
         </div>
-    )
+      </VisibilitySensor>
+      <VisibilitySensor
+        onChange={(isVisible) => startCounting(isVisible, setCount2, 500)}
+        partialVisibility
+      >
+        <div className="flex flex-col w-full items-center justify-center">
+          <h1 className="text-xl md:text-5xl font-medium text-[#E2012D] font-oswald">{count2}</h1>
+          <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">
+            Achievements
+          </div>
+        </div>
+      </VisibilitySensor>
+      <VisibilitySensor
+        onChange={(isVisible) => startCounting(isVisible, setCount3, 9000)}
+        partialVisibility
+      >
+        <div className="flex flex-col w-full items-center justify-center">
+          <h1 className="text-xl md:text-5xl font-medium text-[#E2012D] font-oswald">{count3}</h1>
+          <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">
+            National Participation
+          </div>
+        </div>
+      </VisibilitySensor>
+      <VisibilitySensor
+        onChange={(isVisible) => startCounting(isVisible, setCount4, 1000)}
+        partialVisibility
+      >
+        <div className="flex flex-col w-full items-center justify-center">
+          <h1 className="text-xl md:text-5xl font-medium text-[#E2012D] font-oswald">{count4}</h1>
+          <div className="text-base md:text-2xl font-light text-white font-maven w-full mt-4 text-center">
+            International Participation
+          </div>
+        </div>
+      </VisibilitySensor>
+    </div>
+  );
 }
