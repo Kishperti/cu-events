a23294a0312b28f82fcfe6ad6ba9565b0429264a
Anshika <anshikathakur596@gmail.com>
2024-01-18 23:32:46 +0530

    Update About.jsx

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -12,13 +12,16 @@ const About = () => {
       <div className="flex flex-row items-center justify-center w-full relative mt-6 lg:mt-20 h-full">
         <img src={about} alt="" className="h-[90vh] w-1/2 hidden lg:block" />
         <div className="grid place-items-center px-2 rounded-lg w-[90%] lg:w-[60%] lg:-ml-[20%] bg-[#000000d4]">
-          <p className="text-lg lg:text-xl py-8 text-justify w-full px-7 leading-8 lg:px-12 text-white opacity-80 font-maven z-20">
+          <p className="hidden lg:grid place-items-centertext-lg lg:text-xl py-8 text-justify w-full px-7 leading-8 lg:px-12 text-white opacity-80 font-maven z-20">
             Chandigarh University Academic Competitions, where innovation converges with education to create an unparalleled academic spectacle. Our team is a dynamic collective of visionaries, strategists, and go-getters who are dedicated to orchestrate events that transcend conventional boundaries. We are an ensemble of dedicated professionals, passionate about creating transformative experiences through hackathons, workshops, and various academic events
             <br />
             <br />
             Welcome to CAC, the pulse of academic vibrancy at Chandigarh University. We're dedicated to inspiring and empowering through diverse academic competitions, hackathons, and workshops. Our inclusive community values every voice, fostering an environment where curiosity thrives.
             CAC is more than events; it's a collaborative space, connecting students, faculty, and industry experts. Behind our seamless experiences is a committed team pushing the boundaries of academic engagement. Whether you're a student showcasing skills or an industry expert contributing, CAC invites you to join our journey of exploration and innovation. Welcome to a legacy of academic excellence at CAC.
 
+          </p>
+          <p className="text-lg lg:text-xl lg:hidden py-8 text-justify w-full px-7 leading-8 lg:px-12 text-white opacity-80 font-maven z-20">
+            Chandigarh University Academic Competitions, where innovation converges with education to create an unparalleled academic spectacle. Our team is a dynamic collective of visionaries, strategists, and go-getters who are dedicated to orchestrate events that transcend conventional boundaries. We are an ensemble of dedicated professionals, passionate about creating transformative experiences through hackathons, workshops, and various academic events
           </p>
         </div>
       </div>
